408566a8c781dcc478f74413d21bc91cb392cc3d
Apolo Sol Invictus <ronnywoods77@gmail.com>
2025-08-17 18:25:38 -0600

    Update pdo.php

--- assets/php/pdo.php
+++ assets/php/pdo.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:redws.database.windows.net,1433; Database = redws", "wstudio", "{Wswslogic777}");
+    $conn = new PDO("sqlsrv:server = tcp:redws.database.windows.net,1433; Database = WStudio", "wstudio", "{Wswslogic777}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,8 +10,9 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "wstudio", "pwd" => "{Wswslogic777}", "Database" => "redws", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "wstudio", "pwd" => "{Wswslogic777}", "Database" => "WStudio", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:redws.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
 ?>
+
